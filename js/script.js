81f01fa44b98a68ab5ccb598e0077b1c99bdc916
svasquezb <svasquezb2@gmail.com>
2024-04-19 19:52:33 -0400

    Modificacion login

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-// Agregar evento para abrir el modal del carrito
-const addToCartButtons = document.querySelectorAll('.btn-add-to-cart');
+// Inicializar variables
 const shoppingCartModal = new bootstrap.Modal(document.getElementById('shoppingCartModal'));
 const cartList = document.getElementById('cartList');
 const totalPrice = document.getElementById('totalPrice');
 
+// Arreglo para almacenar los productos en el carrito
 let cart = [];
 
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const productCard = button.closest('.card');
-    const productTitle = productCard.querySelector('.card-title').textContent;
-    const productPrice = parseFloat(productCard.querySelector('.price').textContent.replace('$', ''));
+// Función para agregar un producto al carrito
+function addToCart(product) {
+  // Buscar si el producto ya está en el carrito
+  const existingProduct = cart.find((p) => p.title === product.title);
 
-    const existingProduct = cart.find(item => item.title === productTitle);
-    if (existingProduct) {
-      existingProduct.quantity++;
-    } else {
-      cart.push({ title: productTitle, price: productPrice, quantity: 1 });
-    }
+  if (existingProduct) {
+    // Si el producto ya está en el carrito, aumentar la cantidad
+    existingProduct.quantity++;
+  } else {
+    // Si el producto no está en el carrito, agregarlo con cantidad 1
+    cart.push({ ...product, quantity: 1 });
+  }
 
-    renderCart();
-    shoppingCartModal.show();
-  });
-});
+  // Actualizar el contenido del carrito en la página
+  renderCart();
 
+  // Mostrar el modal del carrito
+  shoppingCartModal.show();
+}
+
+// Función para renderizar el contenido del carrito en la página
 function renderCart() {
   cartList.innerHTML = '';
   let total = 0;
@@ -40,9 +43,21 @@ function renderCart() {
   totalPrice.textContent = `$${total.toFixed(2)}`;
 }
 
+// Agregar event listeners a los botones "Agregar al carrito"
+const addToCartButtons = document.querySelectorAll('.btn-add-to-cart');
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const productCard = button.closest('.card');
+    const productTitle = productCard.querySelector('.card-title').textContent;
+    const productPrice = parseFloat(productCard.querySelector('.price').textContent.replace('$', ''));
+
+    const product = { title: productTitle, price: productPrice };
+    addToCart(product);
+  });
+});
+
 // Efecto de desplazamiento suave al hacer clic en enlaces de la navegación
 const navLinks = document.querySelectorAll('.navbar-nav a');
-
 navLinks.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
